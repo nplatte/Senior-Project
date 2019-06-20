2b50951e0625d323f3f3cb7ab1c0dd7f41d9b50f
Nathan Platte <nathanplatte24@gmail.com>
2019-06-20 14:58:57 -0500

    Added FT structure, started an app for the teacher side of site

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,29 @@
 from selenium import webdriver
-from unittest import TestCase
+from django.test import LiveServerTestCase
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class TestCreateNewClass(LiveServerTestCase):
 
-class TestCreateNewClass(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-    pass
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_admin_can_make_other_admins(self):
+        # An admin logs into the Django admin page
+        # The admin then has a list of other users to make admins
+        # The admin then puts in the first name
+        # The first email
+        # and the password for the new admin
+        pass
+
+    def test_create_new_class(self):
+        # An admin user wants to create a new class page using the html file with student information
+        # They go to the django admin site
+        # They log in using their credentials
+        # The admin user selects the create class option on the page
+        # The admin is taken to a page where they choose to upload a file in html format
+        # After puting in more information, they are taken to their new class page displaying student information like numbers, names, and emails
+        # This page has the course title, the term, and prefessor name on the top
+        pass
---
+++ socrates_teacher_side/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SocratesTeacherSideConfig(AppConfig):
+    name = 'socrates_teacher_side'

a28fc239c83563247e93edaa2ea4b4b8987adf4c
Nathan Platte <nathanplatte24@gmail.com>
2019-06-24 20:30:16 -0500

    added basic Course info to the Course class

--- Class/models.py
+++ Class/models.py
@@ -6,6 +6,18 @@ from html.parser import HTMLParser
 class Course(models.Model):
     
     Class_File = models.FileField(upload_to='class_htmls')
+    
+    def __init__(self):
+        models.Model.__init__(self)
+        self.code = None
+        
+
+    def create(self, file):
+        self.Parser = MyHTMLParser()
+        self.Parser.feed_file(file)
+        self.Parser.sort_data_list('\t\t\t', '\t\t')
+        course_info = self.Parser.data_list[0][1].split(' | ')
+        self.code = course_info[2]
 
 
 class MyHTMLParser(HTMLParser):
@@ -31,16 +43,26 @@ class MyHTMLParser(HTMLParser):
             self.feed(line)
 
     def print_data(self):
-        print(self.data_list)
+        for student in self.data_list:
+            print(student)
 
     def sort_data_list(self, start_char, stop_char):
-        new_list = []
-        append = False
+        # Sorts the data list compiling all of the data for each student into sperate lists
+        new_data_list = []
+        will_append = False
         for entry in self.data_list:
             if entry == start_char:
-                append = True
+                will_append = True
+                student = []
             elif entry == stop_char:
-                append = False
-            if append and entry is not start_char:
-                new_list.append(entry)
-        return new_list
+                if will_append == True:
+                    new_data_list.append(student)
+                will_append = False
+            if will_append and entry is not start_char:
+                student.append(entry)
+        self.data_list = new_data_list
+
+
+class Student:
+
+    pass
--- Class/tests.py
+++ Class/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
-from .models import Course, MyHTMLParser
+from .models import Course, MyHTMLParser, Student
 from os import listdir
 
 
 class TestCourseModel(TestCase):
     
-    pass
+    def test_course_has_title(self):
+        test_class = Course()
+        test_class.create('C:\\Users\\nplat\\OneDrive\\Desktop\\Senior Project\\Class\\test_class_htmls\\original_file.xls')
+        self.assertEqual(test_class.code, 'CS 260 01')
+
 
 class TestMyHTMLParser(TestCase):
 
@@ -20,11 +24,17 @@ class TestMyHTMLParser(TestCase):
     def test_sort_list_based_on_start_stop(self):
         HtmlPars = MyHTMLParser()
         HtmlPars.data_list = ['a', 'start', 'b', 'o', 'stop', 'a', 'stop', 'start', 'b', 'stop', 'a']
-        slist_key = ['b', 'o', 'b']
-        sorted_list = HtmlPars.sort_data_list('start', 'stop')
-        self.assertEqual(slist_key, sorted_list)
+        slist_key = [['b', 'o'], ['b']]
+        HtmlPars.sort_data_list('start', 'stop')
+        self.assertEqual(slist_key, HtmlPars.data_list)
 
     def test_list_has_course_info(self):
         HtmlPars = MyHTMLParser()
         HtmlPars.feed_file('C:\\Users\\nplat\\OneDrive\\Desktop\\Senior Project\\Class\\test_class_htmls\\original_file.xls')
-        
+        HtmlPars.sort_data_list('\t\t\t', '\t\t')
+        self.assertIn(['\t\t\t', 'Class List 2019 May Term | Undergraduate | CS 260 01 | Introduction to Computer Graphics (17 students)'], HtmlPars.data_list)
+
+
+class TestStudentModel(TestCase):
+
+    pass

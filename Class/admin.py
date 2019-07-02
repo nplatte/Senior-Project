c32d4c0302df4fed9b3bca2201ab5ab2130b1558
Nathan Platte <nathanplatte24@gmail.com>
2019-07-01 21:20:55 -0500

    Switched site from fields to fieldsets

--- Class/admin.py
+++ Class/admin.py
@@ -7,47 +7,42 @@ class CourseAdminModelForm(forms.ModelForm):
     class Meta:
 
         model = Course
+        
         fields = [
             'Class_File', 'title', 'code', 'term'
-
-        ]  
+        ] 
 
 class CourseAdmin(admin.ModelAdmin):
 
     form = CourseAdminModelForm
 
-    fields = ['Class_File', 'title', 'code', 'term']
+    fieldsets = (
+        ('Course Information', {
+            'fields': ('Class_File', 'title', 'code', 'term')
+        }),
+    )
+
+    #fields = ['Class_File', 'title', 'code', 'term']
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
         obj.create()
 
-    '''def add_view(self, request, extra_content=None):
-        self.fields = ('Class_File')
-        return super(CourseAdmin, self).add_view(request)
-
-    def change_view(self, request, object_id, extra_content=None):
-         self.fields = ('Class_File', 'title', 'code', 'term')
-         return super(CourseAdmin,self).change_view(request,object_id)'''
-
-    def get_fields(self, request, obj=None):
+    def get_fieldsets(self, request, obj=None):
         if not obj:
-            fields=('Class_File',)
+            fieldsets = (
+                ('Course Information', {
+                    'fields': ('Class_File',)
+                }),
+            )
+            #fields = ('Class_File',)
         else:
-            fields =('term', 'title', 'code')
-        return fields
-
-    '''def get_form(self, request, obj=None, **kwargs):
+            fieldsets = (
+                ('Course Information', {
+                    'fields': ('title', 'code', 'term')
+                }),
+            )
+            #fields = ('title', 'code', 'term')
+        return fieldsets
         
-        if obj: 
-            kwargs['fields'] = ['Class_File', 'title', 'term', 'code']
-        else:
-            kwargs['fields'] = ['Class_File', 'title', 'term', 'code']
-        return super(CourseAdmin, self).get_form(request, obj, **kwargs)'''
-        
-    
-        
-        
-        
-
 admin.site.register(Course, CourseAdmin)

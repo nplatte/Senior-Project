88642efa6ef9eda14db14cb699e6c406e6186220
Nathan Platte <nathanplatte24@gmail.com>
2019-07-15 09:57:02 -0500

    added basic view for student profiles

--- student_view/views.py
+++ student_view/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Class.models import Course
 
 def home_page(request):
     return render(request, 'home.html')
 
 def profile_page(request):
-    return render(request, 'student_profile.html')
+    current_user = request.user
+    #print('\n' + current_user.first_name + current_user.last_name + '\n')
+    course_list = Course.objects.filter(
+        students__name=current_user.first_name + ' ' + current_user.last_name
+        )
+    some_class = course_list.first()
+    return render(
+        request, 
+        'student_profile.html', 
+        { 
+        'user': current_user , 
+        'student_courses': course_list ,
+        }
+        )

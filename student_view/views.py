efd103a35957bd9776cc06ba2f0a68889e223daa
Nathan Platte <nathanplatte24@gmail.com>
2019-07-22 21:30:36 -0500

    Made sure users had to be logged in to see pages

--- student_view/views.py
+++ student_view/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from Class.models import Course, Assignment
 from datetime import date
 
 def home_page(request):
     return render(request, 'student_view/home.html', )
 
+@login_required(login_url='/student/accounts/login/')
 def profile_page(request):
     current_user = request.user
     course_list = _get_course_list(request)
@@ -23,11 +25,13 @@ def profile_page(request):
         }
         )
 
+@login_required(login_url='/student/accounts/login/')
 def assignment_page(request, assignment_title):
     course_list = _get_course_list(request)
     current_assignment = Assignment.objects.get(title=assignment_title)
     return render(request, 'student_view/assignment.html', {'assignment':current_assignment, 'student_courses': course_list})
 
+@login_required(login_url='/student/accounts/login/')
 def course_page(request, course_title):
     course_list = _get_course_list(request)
     current_course = Course.objects.get(title=course_title)
@@ -40,18 +44,22 @@ def course_page(request, course_title):
         'courses_assignments': course_assignments
         })
 
+@login_required(login_url='/student/accounts/login/')
 def grade_page(request):
     course_list = _get_course_list(request)
     return render(request, 'student_view/grades.html', {'student_courses': course_list})
 
+@login_required(login_url='/student/accounts/login/')
 def handout_page(request):
     course_list = _get_course_list(request)
     return render(request, 'student_view/handouts.html', {'student_courses': course_list})
 
+@login_required(login_url='/student/accounts/login/')
 def discussion_page(request):
     course_list = _get_course_list(request)
     return render(request, 'student_view/discussion.html', {'student_courses': course_list})
 
+@login_required(login_url='/student/accounts/login/')
 def _get_course_list(request):
     current_user = request.user
     course_list = Course.objects.filter(

2e1c8150256ebd4973dd37671c488204b117ecc4
Nathan Platte <nathanplatte24@gmail.com>
2019-08-14 09:19:01 -0500

    Added grade model

--- teacher_view/models.py
+++ teacher_view/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Class.models import Student, Course
 
-# Create your models here.
+class Grade(models.Model):
+
+    student = models.ManyToManyField(Student)
+    course = models.ManyToManyField(Course)
+    grade_file = models.FileField(upload_to='course_grades')
+    letter_grade = models.CharField(default='', max_length=5)
+
+    def parse_grade_file(self):
+        pass

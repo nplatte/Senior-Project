04759c2abde73f9153bf5554614cecaab97b0b50
nplatte <77999574+nplatte@users.noreply.github.com>
2023-10-25 19:17:36 -0400

    removed unneeded modules

--- functional_tests/test_teacher_pages.py
+++ functional_tests/test_teacher_pages.py
@@ -2,9 +2,6 @@ from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from time import sleep
-from os import remove
-from Class.models import Course
 from django.contrib.auth.models import User
 
 class TestTeacherCreateNewClass(LiveServerTestCase):

a059bc6f40c8f370d5de6b4be957721dabd2d028
nplatte <77999574+nplatte@users.noreply.github.com>
2023-10-25 19:17:09 -0400

    refactored test to check for teacher file and operate off login page

--- functional_tests/test_teacher_pages.py
+++ functional_tests/test_teacher_pages.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from time import sleep
 from os import remove
 from Class.models import Course
+from django.contrib.auth.models import User
 
-class TestCreateNewClass(LiveServerTestCase):
+class TestTeacherCreateNewClass(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,56 +16,40 @@ class TestCreateNewClass(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def admin_login(self, username, password):
+    def teacher_login(self, username='new', password='password'):
         username_input = self.browser.find_element_by_name('username')
         username_input.send_keys(username)
         password_input = self.browser.find_element_by_name('password')
         password_input.send_keys(password)
         password_input.send_keys(Keys.ENTER)
 
-    def test_admin_can_make_other_admins(self):
-        # The admin opens their browser
-        self.browser.get('http://localhost:8000')
-
-        # An admin goes to the Django admin page
-        self.browser.get('http://localhost:8000/admin/')
-        self.assertIn('admin', self.browser.title)
-
-        # The admin logs into the admin page
-        self.admin_login('nathan.platte', 'Sparta12456')
-        sleep(1)
-        self.assertNotIn('OperationalError ', self.browser.title)
-
     def test_teacher_can_create_new_class(self):
         # A teacher wants to log in to create a new class
         # they go to the Wartburg MCSP Website and see a log in page
+        User.objects.create_user('new', 'new@gmail.com', 'password')
         # They enter the log in information and are taken to the staff view of the website
         self.assertIn('Wartburg MCSP Teachers', self.browser.title)
-
-        # They log in using their credentials
-        self.admin_login('nathan.platte', 'Sparta12456')
-        sleep(1)
-        self.assertNotIn('OperationalError ', self.browser.title)
-
-        # The admin user selects the create class option on the page
-        self.browser.get('http://localhost:8000/admin/Class/course/add/')
-
-
-        # The admin is taken to a page where they choose to upload an .xls file and no other information        
-        # the admin chooses the file and uploads it and presses the save button
+        # they see a nav bar on the top of the page, this displays a Home button, Courses button, an Assignments button, and a Grades Button
+        home_btn = self.browser.find_element(By.ID, 'nav-home')
+        courses_btn = self.browser.find_element(By.ID, 'nav-courses')
+        assignments_btn = self.browser.find_element(By.ID, 'nav-assignments')
+        grades_btn = self.browser.find_element(By.ID, 'nav-grades')
+        # they click the courses Button
+        courses_btn.click()
+        # This takes them to a new page where they see all the courses with a break down of students and recent activity
+        courses = self.browser.find_elements(By.CLASS_NAME, 'course-title')
+        self.assertEqual(len(courses), 0)
+        self.assertEqual('Courses', self.browser.title)
+        # At the top is a button for new courses
+        new_course_btn = self.browser.find_element(By.ID, 'new-course-btn')
+        # They click this button and a window pops up on screen asking for a file upload
+        new_course_btn.click()
+        # They select the course file from their computer and press enter
         file_upload = self.browser.find_element_by_name('Class_File')
         file_upload.send_keys('C:\\Users\\nplat\\OneDrive\\Desktop\\Senior Project\\Class\\test_class_htmls\\cs_220.xls')
         save_course = self.browser.find_element_by_name('_save')
         save_course.click()
-
-        
-        saved_course = Course.objects.all().first()
-        self.assertEqual(saved_course.title, 'Introduction to Computer Graphics')
-        self.assertEqual(saved_course.term, '2019 May Term')
-        self.assertEqual(saved_course.code, 'CS 260 01')
-        
-        remove('C:\\Users\\nplat\\OneDrive\\Desktop\\Senior Project\\class_htmls\\cs_220.xls')
-
-        # They are shown a display page, previewing the information
-        self.assertEqual(self.browser.current_url, 'http://localhost:8000/admin/Class/course/preview/')
-        # This page has the course title, the term, and prefessor name on the top and the students displayed below
+        # the back end processes the file and generates a new course page from the file
+        # they see the students in the course and navigate to courses and see the course listed after refreshing.
+        courses = self.browser.find_elements(By.CLASS_NAME, 'course-title')
+        self.assertEqual(len(courses), 1)

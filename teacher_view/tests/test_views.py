f1c10bf87f75eabb06ec834540ca83d9911da2eb
Nathan Platte <nplatte0@gmail.com>
2023-11-03 19:46:33 -0500

    added test to check for template and change template name in urls.py

--- teacher_view/tests/test_views.py
+++ teacher_view/tests/test_views.py
@@ -2,12 +2,18 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User, Group
 
+
+def _add_staff_user():
+    test_user = User.objects.create_user('new', 'new@gmail.com', 'password')
+    staff_group = Group.objects.create(name='staff')
+    staff_group.user_set.add(test_user)
+    return test_user
+
+
 class TestHomePage(TestCase):
 
     def setUp(self):
-        test_user = User.objects.create_user('new', 'new@gmail.com', 'password')
-        staff_group = Group.objects.create(name='staff')
-        staff_group.user_set.add(test_user)
+        test_user = _add_staff_user()
         self.client.force_login(test_user)
 
     def test_home_page_uses_right_template(self):
@@ -18,9 +24,7 @@ class TestHomePage(TestCase):
 class TestProfilePage(TestCase):
 
     def setUp(self):
-        test_user = User.objects.create_user('new', 'new@gmail.com', 'password')
-        staff_group = Group.objects.create(name='staff')
-        staff_group.user_set.add(test_user)
+        test_user = _add_staff_user()
         self.client.force_login(test_user)
 
     def test_profile_page_uses_right_template(self):
@@ -31,4 +35,12 @@ class TestProfilePage(TestCase):
 class TestAddCoursePage(TestCase):
 
     def setUp(self):
-        pass
+        test_user = _add_staff_user()
+        self.client.force_login(test_user)
+
+    def test_page_uses_right_template(self):
+        request = self.client.get(reverse('staff_add_course_page'), follow=True)
+        self.assertTemplateUsed(request, 'teacher_view/create_class.html')
+
+    def test_page_passes_current_courses_to_navbar(self):
+        pass 

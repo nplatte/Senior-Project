75ea214dcf6738eb87f1c2cfdb98311599a9a960
Nathan Platte <nplatte0@gmail.com>
2023-12-02 04:50:01 -0600

    finished merge resolving

--- student_view/tests.py
+++ student_view/tests.py
@@ -6,16 +6,3 @@ from django.template.loader import render_to_string
 from .views import home_page, course_page
 
 
-class HomePageTest(TestCase):
-
-    def test_root_url(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-class CoursePageTest(TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_course_title_displayed(self):
-        pass

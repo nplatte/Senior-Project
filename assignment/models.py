42900ca688f487a1db9801e12db35b441a820142
Nathan Platte <nplatte0@gmail.com>
2023-12-25 08:28:52 -0600

    added Assignment model and assignment tests

--- assignment/models.py
+++ assignment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from course.models import Course
 
-# Create your models here.
+class Assignment(models.Model):
+    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True, related_name='assignments')
+    title = models.CharField(max_length=50, default='')
+    description = models.TextField(default='')
+    due_date = models.DateTimeField(blank=True, null=True)
+    display_date = models.DateTimeField(blank=True, null=True)
+
+    def __repr__(self) -> str:
+        return str(self.title)

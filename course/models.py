73dd63a01e87a41a02c8cf4a7d2d247f3ea0f0a0
Nathan Platte <nplatte0@gmail.com>
2023-12-25 07:17:43 -0600

    updated link on courses page and added __str__ method

--- course/models.py
+++ course/models.py
@@ -47,5 +47,8 @@ class Course(models.Model):
 
     def get_absolute_url(self):
         return reverse('view_course_page', kwargs={'course_id': self.pk})
+    
+    def __str__(self):
+        return self.title
 
   

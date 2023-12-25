690f5a8d9f38d7338b81a5cd22b2d7b520e98d9a
Nathan Platte <nplatte0@gmail.com>
2023-12-25 15:35:08 -0600

    added a few minor test steps, changed nav bar link, added course create title

--- functional_tests/inherits.py
+++ functional_tests/inherits.py
@@ -13,21 +13,32 @@ from time import sleep
 
 class BasicSeleniumTest(StaticLiveServerTestCase):
 
+    ''' This is the parent class of each of the functional tests, it provides the 
+    setUp and tearDown that is needed for each class. In addition it provides attributes
+    such as self.browser and the base path needed for some of the functional tests.
+
+    '''
+
     def setUp(self) -> None:
+        # each Functional Test ran needs a teacher account that can be logged into
         self.test_user = User.objects.create_user('new', 'new@gmail.com', 'password')
         staff_group = Group.objects.create(name='staff')
         staff_group.user_set.add(self.test_user)
+        # it also needs a browser window
         self.browser = webdriver.Firefox()
+        # and a base path of where to pull the sample course files from
         self.base_path = f'{getcwd()}\\teacher_view\\test_class_htmls'
         return super().setUp()
     
     def tearDown(self) -> None:
+        # looks in the file upload location and deletes the file if it exists
         upload_file = f'{getcwd()}\\class_htmls\\CS_260.xls'
         if path.exists(upload_file):
             remove(upload_file)
         return super().tearDown()
     
     def _teacher_login(self, username='new', password='password'):
+        # this is to be used on the login screen to log in the test user
         username_input = self.browser.find_element(By.ID, 'username')
         username_input.send_keys(username)
         password_input = self.browser.find_element(By.ID, 'password')
@@ -36,6 +47,7 @@ class BasicSeleniumTest(StaticLiveServerTestCase):
         sleep(3)
     
     def _create_course(self, instructor=None):
+        # this creates a course using the InMemoryUploadedFile as the course file
         if not instructor:
             instructor = self.test_user
         file = open(f'{self.base_path}\\CS_260.xls')
--- functional_tests/test_courses.py
+++ functional_tests/test_courses.py
@@ -15,47 +15,60 @@ class TestTeacherClass(BasicSeleniumTest):
 
     def tearDown(self):
         self.browser.quit()
-        return super().tearDown()
+        super().tearDown()
 
     def test_teacher_can_create_new_class(self):
         # A teacher wants to log in to create a new class
         # they go to the Wartburg MCSP Website and see a log in page
-        # They enter the log in information and are taken to the staff view of the website
         self.assertIn('Log In', self.browser.title)
+        # They enter the log in information and are taken to the staff view of the website
         self._teacher_login()
         self.assertIn('Wartburg MCSP Teachers', self.browser.title)
-        # they see a nav bar on the top of the page, this displays a Home button, Courses button, an Assignments button, and a Grades Button
-        chain = ActionChains(self.browser)
+        # they see a nav bar on the top of the page, this displays a Home button and Courses button 
         home_btn = self.browser.find_element(By.ID, 'nav-home')
         courses_btn = self.browser.find_element(By.ID, 'nav-courses')
         # they move to the courses dropdown and reveal other options
+        chain = ActionChains(self.browser)
         chain.move_to_element(courses_btn).perform()
         add_course_btn = self.browser.find_element(By.ID, 'add-course')
-        curr_courses_btn = self.browser.find_element(By.ID, 'curr-courses')
-        # They move to see the current courses
-        curr_courses_btn.click()
-        # uh oh! They don't see any
+        all_courses_link = self.browser.find_element(By.ID, 'all-courses')
+        # They decide to view all of their courses
+        all_courses_link.click()
+        # They see they have no courses
         curr_courses = self.browser.find_elements(By.ID, 'course_title')
         self.assertEqual(len(curr_courses), 0)
         # they decide to fix this by adding a course
+        # they click the add course button
         courses_btn = self.browser.find_element(By.ID, 'nav-courses')
         chain.move_to_element(courses_btn).perform()
         add_course_btn = self.browser.find_element(By.ID, 'add-course')
         add_course_btn.click()
         # This takes them to a new page where they see a form to make courses
+        self.assertEqual(self.browser.title, 'Create Course')
         # At the top is an upload for files
         # They select the course file from their computer and press enter
         file_upload = self.browser.find_element(By.ID, 'source_file_input')
         file_upload.send_keys(f'{getcwd()}\\teacher_view\\test_class_htmls\\CS_260.xls')
-        save_course = self.browser.find_element(By.ID, 'file-submit')
-        save_course.click()
+        create_course = self.browser.find_element(By.ID, 'file-submit')
+        create_course.click()
         # the teacher is automatically taken to a new course page where theysee the new course title and stuff
         course = Course.objects.all()[0]
         self.assertEqual(self.browser.title, course.title)
         self.browser.find_element(By.ID, 'course-title')
+        # They then click view all course to make sure that the new course is there
+        courses_btn = self.browser.find_element(By.ID, 'nav-courses')
+        chain = ActionChains(self.browser)
+        chain.move_to_element(courses_btn).perform()
+        all_courses_link = self.browser.find_element(By.ID, 'all-courses')
+        all_courses_link.click()
+        # they see the new course
+        curr_courses = self.browser.find_elements(By.ID, 'course_title')
+        self.assertEqual(len(curr_courses), 0)
 
     def test_teacher_can_edit_courses(self):
-        # the teacher logs into the website and edit the class they have previously made
+        old_title = 'Introduction to Comp'
+        new_title = 'Introduction to Computer Graphics'
+        # the teacher logs into the website to edit the class they have previously made
         c = self._create_course(self.test_user)
         # they log in
         self.assertEqual('Log In', self.browser.title)
@@ -69,7 +82,7 @@ class TestTeacherClass(BasicSeleniumTest):
         course_link = self.browser.find_element(By.ID, f'course-{c.pk}-link')
         course_link.click()
         # they are taken to the course page  
-        self.assertEqual(self.browser.title, 'Introduction to Comp')
+        self.assertEqual(self.browser.title, old_title)
         # They notice the spelling error and go to correct it
         # they click the edit course info button and are taken to a page with the form info
         link = self.browser.find_element(By.ID, 'edit-course-link')
@@ -83,9 +96,13 @@ class TestTeacherClass(BasicSeleniumTest):
         self.assertEqual(code_edit.get_attribute('value'), c.code)
         # the techer clicks the title edit, clears it and sends the right title in
         title_edit.clear()
-        title_edit.send_keys('Introduction to Computer Graphics')
+        title_edit.send_keys(new_title)
         # The teacher is satisfied and clicks the submit button
         btn = self.browser.find_element(By.ID, 'edit-course-submit')
         btn.click()
-        # the teacher is taken back to the view course page
-        self.assertEqual(self.browser.title, 'Introduction to Computer Graphics')
+        # the teacher is taken back to the view course page with the new title as the browser title
+        self.assertEqual(self.browser.title, new_title)
+        # they see the new course information entered in
+        course_title = self.browser.find_element(By.ID, 'course-title')
+        self.assertNotEqual(course_title.text, old_title)
+        self.assertEqual(course_title.text, new_title)
